f03e2645dcfcb3ebabdfb7879b4fcdf4e1d9c632
ivanbakotin <ivan.bakotin1999@gmail.com>
2021-10-29 15:01:25 +0200

    update

--- Vježba1/main.c
+++ Vježba1/main.c
@@ -1,112 +1,97 @@
 #include <stdio.h>
 #include <string.h>
 
- int mystrlen(char s[]) {
+int mystrlen(char *s) {
     int i;
 
-    for (i = 0; s[i] != '\0'; ++i);
+    for (i = 0; s[i] != '\0'; i++);
 
     return i;
 };
 
-char *mystrcpy(char s1[], char s2[]) {
-    return s1 = s2;
+void mystrcpy(char *s1, char *s2) {
+    int a, b;
+    
+    for (int i = 0; s2[i] != '\0'; i++) {
+        s1[i] = s2[i];
+    }
 };
 
 int mystrcmp(const char *s1, const char *s2){
-    for(;*s1 && *s2 && (*s1 == *s2); s1++, s2++){}
-    if (*s1 - *s2 < 0) return -1;
-    else if (*s1 - *s2 > 0) return 1;
+    int i = 0;
+    while(s1[i] == s2[i] && s1[i]) {i++;}
+
+    if (s1[i] < s2[i]) return -1;
+    else if (s1[i] > s2[i]) return 1;
     else return 0;
 }
 
-void mystrcat(char *s1, char *s2, char *catstring) {
+void mystrcat(char *s1, char *s2) {
      
-    int k=0;
-     
-    while(*s1 != '\0') {
-           catstring[k] = *s1;
-           k++;
-           s1++;         
-    }      
-     
-    while(*s2 != '\0') {
-           catstring[k] = *s2;
-           k++;
-           s2++;
-    }
+    int i, len1, len2;
+    len1 = mystrlen(s1);
+    len2 = mystrlen(s2) + len1 + 1;
 
-    catstring[k] = '\0';    
+    for (len1, i = 0; len1 < len2; len1++, i++) {
+        s1[len1] = s2[i];
+    }
 }
 
-char* mystrstr(char *s, char *subs) {
-  char *a = s;
-  char *b = subs;
+char* mystrstr(char* s1, char* s2) {
 
-  for( ; *s != '\0'; *s++) {
-    if(*s != *b) {
-      continue;
+    char* p = s1;
+    int i, j;
+
+    for (i = 0; s1[i] != '\0'; i++) {
+        for (j = 0; s1[i + j] == s2[j]; j++) {
+            p = &s1[i];
+            if (s2[j + 1] == '\0') {        
+                return p;
+            }
+        }
     }
 
-    a = s;
-    while(1) {
-      if(*b == '\0') {
-        return s;
-      } else if(*a++ != *b++) {
-        break;
-      }
-    }
-    b = subs;
-  }
-
-  return (char *) NULL;
+    return p;
 }
 
-void revstr(char *str1) {  
+void reverse(char* s1, char* s2) {
 
-    int i, len, temp;  
-    len = mystrlen(str1); 
-      
-    for (i = 0; i < len/2; i++) {  
-        temp = str1[i];  
-        str1[i] = str1[len - i - 1];  
-        str1[len - i - 1] = temp;  
-    }  
-}  
+    int i;
+
+    int len = mystrlen(s1);
+
+    for (i = 0; s1[i] != '\0'; ++i) {
+ 
+        s2[i] = s1[len - 1 - i];
+    }
+
+    s2[i] = '\0';
+}
 
 void main(void) {
-    //char string1[] = "a";
-    //char string2[] = "AAA";
-    char string3[] = "Hello";
-    //char catstring[sizeof(string2) + sizeof(string3)];
-//
+    char string1[30] = "aa";
+    char string2[30] = "Hello";
+    char string3[30] = "aaabbaa";
+
     //printf("My strlen: %d\n", mystrlen(string1));
     //printf("C strlen: %d \n", strlen(string1));
-//
-    //printf("My strcpy: %s \n", mystrcpy(string1, string2));
-    //printf("C strcpy: %s \n", strcpy(string1, string2));
-//
+
+    //mystrcpy(string1, string3);
+    //printf("Mystrcpy: %s\n", string1);
+    //strcpy(string1, string3);
+    //printf("Strcpy: %s\n", string1);
+
     //printf("My strcmp: %d \n", mystrcmp(string2, string3));
     //printf("C strcmp: %d \n", strcmp(string2, string3));
-//
-    //mystrcat(string2, string3, catstring);
-    //printf("%s\n", catstring);
+
+    //mystrcat(string2, string3);
+    //printf("Mystrcat: %s\n", string2);
     //strcat(string2, string3);
-    //printf("%s\n", catstring);
+    //printf("Strcat: %s\n", string2);
 
-    char big_string[] = "Hello";
-    char sub_string[] = "ell";
+    //char *res = mystrstr(string2, string3);
+    //printf("%d\n", res);
 
-    char *str = big_string;
-    char *substr = sub_string;
-
-    char *res = mystrstr(str, substr);
-    if(res != (char *) NULL) {
-      printf("Substring match starting at: %c\n", *mystrstr(str, substr));
-    } else {
-      printf("There was no match found.\n");
-    }
-
-    revstr(string3);  
-    printf ("After reversing the string: %s\n", string3);  
+    reverse(string2, string3);  
+    printf("After reversing the string: %s\n", string3);  
 };
